0543d102cde242f532e843a0ffbf652ff2ddc1a2
Hermivaldo <hermivaldo.braga@gmail.com>
2016-03-14 22:48:01 -0300

    Exemplo WebServiceRest Java

---
+++ src/com/hermivaldo/webservice/ConvertTemp.java
@@ -0,0 +1,28 @@
+package com.hermivaldo.webservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+
+@Path("/converter")
+public class ConvertTemp {
+
+	@GET
+	@Produces("application/json")
+	public Response convertCtoF() {
+
+		JSONObject jsonObject = new JSONObject();
+		Double fahrenheit = 98.24;
+		Double celsius;
+		celsius = (fahrenheit - 32) * 5 / 9;
+		jsonObject.put("F Value", fahrenheit);
+		jsonObject.put("C Value", celsius);
+
+		//String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		return Response.status(200).entity(jsonObject.toString()).build();
+	}
+
+}
